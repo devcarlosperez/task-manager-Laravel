6f52b3c4ecbb8afd134cdd093c97c11da62af0ce
devcarlosperez <carlosperezsantana04@gmail.com>
2025-10-10 17:46:27 +0100

    Created controller for the model Task and created routes

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Models\Task;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+      $this->middleware('auth');
+    }
+
+    public function index()
+    {
+      $tasks = Auth::user()->tasks()->get();
+      return view('tasks.index', compact('tasks'));
+    }
+
+    public function create()
+    {
+      return view('tasks.create');
+    }
+
+    public function store(Request $request)
+    {
+      $task = new Task;
+      $task->name = $request->input('name');
+      $task->description = $request->input('description');
+      $task->priority = $request->input('priority');
+      $task->user_id = Auth::id();
+      $task->save();
+
+      return redirect()->route('tasks.index');
+    }
+
+    public function edit($id)
+    {
+      $task = Auth::user()->tasks()->find($id);
+      return view('tasks.edit', compact('task'));
+    }
+
+    public function update(Request $request, $id)
+    {
+      $task = Auth::user()->tasks()->find($id);
+
+      $task->name = $request->input('name');
+      $task->description = $request->input('description');
+      $task->priority = $request->input('priority');
+      $task->update();
+
+      return redirect()->route('tasks.index');
+    }
+
+    public function destroy($id)
+    {
+      $task = Auth::user()->tasks()->find($id);
+      $task->delete();
+      
+      return redirect()->route('tasks.index');
+    }
+}
