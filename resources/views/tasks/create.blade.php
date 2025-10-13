1d3e37eded883372c3f627d62dd022263719f293
devcarlosperez <carlosperezsantana04@gmail.com>
2025-10-13 19:14:14 +0100

    created view index

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,27 +1,60 @@
-@extends('layouts.segundoapp')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-    <h1>Create a new task</h1>
-    <form method="POST" action="{{ route('tasks.store') }}">
-        @csrf
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" name="name" class="form-control" id="name" placeholder="Enter task name">
-        </div>
-        <div class="form-group">
-            <label for="description">Description</label>
-            <input type="text" name="description" class="form-control" id="description"
-                placeholder="Enter task description">
-        </div>
-        <div class="form-group">
-            <label for="priority">Priority</label>
-            <select name="priority" id="priority" class="form-control">
-                <option value="low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="high">High</option>
-            </select>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-    <a href="{{ route('tasks.index') }}">Back to the list of tasks</a>
-@endsection
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        @include('layouts.navigation')
+
+        <!-- Page Heading -->
+        @isset($header)
+            <header class="bg-white dark:bg-gray-800 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endisset
+        <main>
+            <h1>Create a new task</h1>
+            <form method="POST" action="{{ route('tasks.store') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" class="form-control" id="name"
+                        placeholder="Enter task name">
+                </div>
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <input type="text" name="description" class="form-control" id="description"
+                        placeholder="Enter task description">
+                </div>
+                <div class="form-group">
+                    <label for="priority">Priority</label>
+                    <select name="priority" id="priority" class="form-control">
+                        <option value="low">Low</option>
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            <div>
+                <a href="{{ route('tasks.create') }}">Add a new task</a>
+            </div>
+        </main>
+    </div>
+</body>
