5fb86f58235d691f2d760478993fdae95b40e8ff
devcarlosperez <carlosperezsantana04@gmail.com>
2025-10-17 10:45:21 +0100

    Updated styles on the welcome page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,7 +27,12 @@
                     <x-slot name="trigger">
                         <button
                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                            @if (Auth::check())
+                                <div>{{ Auth::user()->name }}</div>
+                            @else
+                                <div>Guest</div>
+                            @endif
+
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
@@ -81,7 +86,7 @@
             <x-responsive-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.index')">
                 {{ __('Tasks') }}
             </x-responsive-nav-link>
-
+            Auth:user()->
             <x-responsive-nav-link :href="route('tasks.create')" :active="request()->routeIs('tasks.create')">
                 {{ __('Create Task') }}
             </x-responsive-nav-link>
